8c16ef97e87559c47b0f8e20f353a6161ca3d848
NikolayNlK <nikitin.nikolay@bk.ru>
2016-10-11 17:26:16 +0800

    Classwork

---
+++ 3024/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		float[] values = new float[6];
+
+		for(int i = 0; i < values.length; i++) {
+			values[i] = scanner.nextFloat();
+		}
+
+		scanner.close();
+
+		float a = getDistance(values[0], values[1], values[2], values[3]);
+		float b = getDistance(values[2], values[3], values[4], values[5]);
+		float c = getDistance(values[4], values[5], values[0], values[1]);
+		
+		System.out.println(a + b + c);
+	}
+
+	private static float getDistance(float x1, float y1, float x2, float y2) {
+		return (float)Math.sqrt(Math.abs(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
+	}
+}
